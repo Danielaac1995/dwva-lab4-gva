89d3d7966ced1e829e1f8ef60248489eae9f39eb
Danielaac1995 <danielaac2020@gmail.com>
2026-04-11 10:49:25 +0000

    Fix: File Upload - validar tipo MIME y extension en upload/low.php

--- vulnerabilities/upload/source/low.php
+++ vulnerabilities/upload/source/low.php
@@ -5,8 +5,17 @@ if( isset( $_POST[ 'Upload' ] ) ) {
 	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
 	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
 
+	// Validate file type and extension
+	$allowed_types = array( 'image/jpeg', 'image/png', 'image/gif' );
+	$allowed_exts  = array( 'jpg', 'jpeg', 'png', 'gif' );
+	$file_type = $_FILES[ 'uploaded' ][ 'type' ];
+	$file_ext  = strtolower( pathinfo( $_FILES[ 'uploaded' ][ 'name' ], PATHINFO_EXTENSION ) );
+
+	if( !in_array( $file_type, $allowed_types ) || !in_array( $file_ext, $allowed_exts ) ) {
+		$html .= '<pre>Only image files (jpg, png, gif) are allowed.</pre>';
+	}
 	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
+	else if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
 		// No
 		$html .= '<pre>Your image was not uploaded.</pre>';
 	}
